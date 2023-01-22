b082b02af593cdd9b2a4d0e9b204ea3eb0e40c5c
kokic <3250679033@qq.com>
2023-01-22 20:34:23 +0800

    clear comment

--- src/parsec.js
+++ src/parsec.js
@@ -59,12 +59,6 @@ Parser.prototype.map = function (morph) {
   )
 }
 
-// let tuple = this.parse(source)
-// if (!tuple) return undefined
-
-// let [a, residue] = tuple
-// return [morph(a), residue]
-
 Parser.prototype.first = proxy(x => x.map(tuple => tuple[0]))
 Parser.prototype.second = proxy(x => x.map(tuple => tuple[1]))
 
@@ -86,25 +80,6 @@ Parser.prototype.follow = function (next) {
   )
 }
 
-//  -> [a, phase1, b, phase2]      (flat )
-// flat version: ((a, _, b, s) => [[a, b], s])(...xs.flat())
-
-// let link1 = new Link(() => this.parse(source)).check()
-// let link2 = new Link(xs => next.parse(xs[1])).check()
-// let morph = xs => xs && ((a, _, b, s) => [[a, b], s])(...xs.flat())
-
-// let tuple1 = this.parse(source)
-// if (!tuple1) return undefined
-
-// let [a, phase1] = tuple1
-
-// let tuple2 = next.parse(phase1)
-// if (!tuple2) return undefined
-
-// let [b, phase2] = tuple2
-// return [[a, b], phase2]
-
-
 /*
  *  tuple1? -> [a, phase1]                 (check)
  *          -> [[a, phase1], tuple2?]      (glue )
@@ -120,19 +95,6 @@ Parser.prototype.skip = function (next) {
   )
 }
 
-// let tuple1 = this.parse(source)
-// if (!tuple1) return undefined
-
-// let [a, phase1] = tuple1
-// let tuple2 = next.get().parse(phase1)
-// if (!tuple2) return undefined
-
-// let [, phase2] = tuple2
-// return [a, phase2]
-
-
-
-
 /*
  *  tuple? -> [a, residue]
  *         -> [a, residue] (check predicate)
@@ -144,13 +106,6 @@ Parser.prototype.check = function (predicate) {
       .exec()
   )
 }
-// let tuple1 = this.parse(source)
-// if (!tuple1) return undefined
-
-// let [a, phase1] = tuple1
-// if (!predicate(a, phase1)) return undefined
-// return tuple1
-
 
 /*
  *  tuple1? -> tuple1 (check)
@@ -161,7 +116,3 @@ Parser.prototype.or = function (next) {
     this.parse(source) || next.parse(source)
   )
 }
-
-// let tuple = this.parse(source)
-// if (tuple) return tuple
-// return next.get().parse(source)
