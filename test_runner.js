772770c0dbb568a31f232459cdf608dc43d4ab49
kokic <3250679033@qq.com>
2023-02-08 11:23:14 +0800

    test pass

--- test_runner.js
+++ test_runner.js
@@ -11,12 +11,14 @@ const ignoreTests = [];
 
 const testDir = "./test/";
 const testDirContents = fs.readdirSync(testDir);
-const testSrc = testDirContents.filter((fileName) =>
-  fileName.split(".").pop() == "tex"
-);
-const testNames = testDirContents.filter((fileName) =>
-  fileName.split(".").pop() == "tex"
-).map((fileName) => fileName.split(".")[0]);
+
+const isTeXFile = fileName => fileName.endsWith(".tex")
+const allTeXFiles = testDirContents.filter(isTeXFile)
+const testSrc = allTeXFiles;
+
+const getTeXName = x => x.substring(0, x.lastIndexOf('.tex'))
+const testNames = allTeXFiles.map(getTeXName);
+
 const enableApplyCalcResults = process.env["TEST_APPLY_RESULTS"] == "1";
 
 const runTest = (
